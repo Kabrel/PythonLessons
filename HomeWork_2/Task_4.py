de00e695cb474aa4ede6d08c792b593736da747e
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-10-14 00:33:18 +0300

    Task 5 done!

--- HomeWork_2/Task_4.py
+++ HomeWork_2/Task_4.py
@@ -6,21 +6,24 @@
 """
 import random
 
-n_count = int(input('Введите количество элементов: '))
-index_list = range(1, n_count+1)
+try:
+    n_count = int(input('Введите количество элементов: '))
+    index_list = range(1, n_count+1)
 
-val_dict = {str(key): random.randint(-n_count, n_count) for key in index_list}
+    val_dict = {str(key): random.randint(-n_count, n_count) for key in index_list}
 
-[print(f'Позиция {k}: значение {v}') for k, v in val_dict.items()]
+    [print(f'Позиция {k}: значение {v}') for k, v in val_dict.items()]
 
-with open('file.txt', "w") as f:
-    for i in index_list:
-        f.write(str(i) + '\n')
+    with open('file.txt', "w") as f:
+        for i in index_list:
+            f.write(str(i) + '\n')
 
-with open('file.txt', 'r') as f:
-    positions = random.sample([i.replace('\n', '') for i in f], 2)
+    with open('file.txt', 'r') as f:
+        positions = random.sample([i.replace('\n', '') for i in f], 2)
 
-print(f'Позиции для перемножения {positions[0]} и {positions[1]}')
+    print(f'Значения для перемножения {val_dict[positions[0]]} и {val_dict[positions[1]]}')
 
-result = val_dict[positions[0]] * val_dict[positions[1]]
-print('Результат =', result)
+    result = val_dict[positions[0]] * val_dict[positions[1]]
+    print('Результат =', result)
+except ValueError:
+    print('Введены не верные данные.')
