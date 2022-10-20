bd77684ae179d0a2a620ed5647ad6145326ae6b9
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-10-20 16:07:51 +0300

    Task 2 update!

--- HomeWork_4/Task_2.py
+++ HomeWork_4/Task_2.py
@@ -2,22 +2,21 @@
 
 """
 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+Так чтобы множетели не повторялись.
 """
 
 number: int = int(input('Введите целое число: '))
 
 
-def find_simple_divider(num: int) -> list[int]:
-    result = []
-    while num % 2 == 0:
-        result.append(2)
-        num = num / 2
-    for i in range(3, int(num ** 0.5) + 1, 2):
+def find_simple_divider(num: int) -> set[int]:
+    result = set()
+    if num % 2 == 0:
+        result.add(2)
+    for i in range(3, num + 1, 2):
         while num % i == 0:
-            result.append(i)
+            result.add(i)
             num = num / i
-    if num > 2:
-        result.append(2)
     return result
 
 
