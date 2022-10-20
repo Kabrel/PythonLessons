b5074cb0e734ef68b6a2b80d91c10399a2bd4fb7
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-10-20 16:15:22 +0300

    Update Task_2.py

--- HomeWork_4/Task_2.py
+++ HomeWork_4/Task_2.py
@@ -13,7 +13,7 @@ def find_simple_divider(num: int) -> set[int]:
     result = set()
     if num % 2 == 0:
         result.add(2)
-    for i in range(3, num + 1, 2):
+    for i in range(3, num + 1, 2):  # брать int(num ** 0.5) вместо num нельзя, ярко выражена ошибка на числах 33, 199
         while num % i == 0:
             result.add(i)
             num = num / i
