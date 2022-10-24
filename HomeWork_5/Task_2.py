008bdb11155f506429985f66a84b6b8910bde538
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-10-25 01:54:57 +0300

    update

--- HomeWork_5/Task_2.py
+++ HomeWork_5/Task_2.py
@@ -1,9 +1,13 @@
 '''
 Создайте программу для игры с конфетами человек против человека.
 
-Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
-Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
-Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+Условие задачи: На столе лежит 2021 конфета. 
+Играют два игрока делая ход друг после друга. 
+Первый ход определяется жеребьёвкой. 
+За один ход можно забрать не более чем 28 конфет. 
+Все конфеты оппонента достаются сделавшему последний ход. 
+Сколько конфет нужно взять первому игроку, 
+чтобы забрать все конфеты у своего конкурента?
 
 a) Добавьте игру против бота
 
@@ -11,14 +15,44 @@ b) Подумайте как наделить бота ""интеллектом"
 '''
 
 from random import randint as rand
-from Task_2_user import Player
+import Task_2_classes as game
 
 
 def create_player():
-    name = input('Как вас зовут пират?: ')
-    return 
+    p_name = input('Как вас зовут пират?: ')
+    if p_name:
+        return game.Player(p_name)
+    else:
+        return game.Player('Немой')
+
+
+def create_bot():
+    return game.Player('Сильвер', human=False)
 
 
 def toss_coin():
     return "Орел" if rand(0,1) == 1 else "Решка"
 
+
+def make_human_turn(player):
+    while True:
+        try:
+            num = int(input('Сколько монет вы кладете в руку?: '))
+            game.Game.take_coins(num)
+            player.take_coin(num)
+            break # продумать отрицательное количество монет
+        except ValueError:
+            pass
+
+def make_bot_turn():
+    pass
+
+def start_game():
+    players_count = int(print('Введите кол-во игроков (1 или 2): '))
+    Player_1 = create_player()
+    if players_count >= 2:
+        Player_2 = create_player()
+    else:
+        Player_2 = create_bot()
+    p_choose = input()
+    toss_coin()
--- HomeWork_5/Task_2_classes.py
+++ HomeWork_5/Task_2_classes.py
@@ -3,17 +3,42 @@
 '''
 
 
+class Game():
+
+    def __init__(self):
+        self.__coins_count = 2021
+
+    def take_coins(self, num:int):
+        if 0 < num <= 28:
+            self.__coins_count -= num
+        elif num == 0:
+            self.__coins_count -= 1
+        else:
+            self.__coins_count -= 28
+    
+    def get_coins_balance(self):
+        print(f'В сундуке осталось {self.__coins_count} монет')
+
+
 class Player():
-    def __init__(self, name = 'Player'):
+    def __init__(self, name = 'Player', human=True):
         self.name = name
+        self.__human = human
         self.__coins = 0 #  защита от читерства. Играйте честно.
     
     def take_coin(self, num: int):
-        if num <= 28:
+        if 0 < num <= 28:
             self.__coins += num
+        elif num == 0:
+            self.__coins += 1
+            print('1 монета прилипла к вашим рукам')
         else:
             self.__coins += 28
             print(f'К сожалению, {num - 28} монет не уместилось в руке и упали обратно.')
     
-    def get_coin_count(self):
+    def get_player_coin_count(self):
         print(f'У игрока {self.name} сейчас {self.__coins} монет.')
+        return self.__coins
+
+
+new_game = Game()
