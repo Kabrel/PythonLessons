d0d34c9f22eaa53527c4f568d4877d9a7a0e4e72
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-10-24 23:35:17 +0300

    Task_5 Done

--- HomeWork_4/Task_3.py
+++ HomeWork_4/Task_3.py
@@ -8,40 +8,31 @@
 
 from random import randint
 
-#k = int(input('Введите целое число > 1: '))
+uni_dict = {"0": "\u2070",
+                "1": "\u00B9",
+                "2": "\u00B2",
+                "3": "\u00B3",
+                "4": "\u2074",
+                "5": "\u2075",
+                "6": "\u2076",
+                "7": "\u2077",
+                "8": "\u2078",
+                "9": "\u2079",
+                "-": "\u207B"
+                }
 
 
 def get_random_value(val):
     return tuple([randint(0, 100), val])
 
 
-'''
-По какой то причине, на машине не работает юникод
-'''
-
-
 def make_unicode_number(data):
-    uni_dict = {"0": u"\u2070",
-                "1": u"\u00B9",
-                "2": u"\u00B2",
-                "3": u"\u00B3",
-                "4": u"\u2074",
-                "5": u"\u2075",
-                "6": u"\u2076",
-                "7": u"\u2077",
-                "8": u"\u2078",
-                "9": u"\u2079",
-                "-": u"\u207B"
-                }
     result = ''.join(str(uni_dict[i]) for i in str(data))
     return result
 
 
 def make_string(degree, val, k):
-    try:
-        exp = make_unicode_number(degree)  # Если функция раскоментирована, будет красивый текст.
-    except:
-        exp = degree
+    exp = make_unicode_number(degree)  # Если функция раскоментирована, будет красивый текст.
     if degree == k:
         if val > 1:
             return f'{val}*x{exp}'
@@ -65,5 +56,3 @@ def set_degree_values(exp):
     result = ' + '.join([make_string(i, v, exp) for v, i in map(get_random_value, range(exp, -1, -1))])
     print(result + ' = 0')
     return result
-
-
--- HomeWork_4/Task_4.py
+++ HomeWork_4/Task_4.py
@@ -7,42 +7,58 @@ from os.path import exists as file_exists
 from random import randint
 import Task_3
 
-file_count = 2
-
 values_dict = {}
 
 
+def make_number_from_unicode(num):
+    inv_dict = {v: k for k, v in Task_3.uni_dict.items()}
+    result = ''.join(str(inv_dict[i]) for i in str(num))
+    return result
+
+
 def check_all_condition(v_data):
-    print(v_data)\
+    result = None
     if len(v_data) > 1:
         if not v_data[1][1:]:
-            return '1', v_data[0]
+            result = ('1', v_data[0])
         else:
-            return v_data[1][1:], v_data[0]
+            result =  (make_number_from_unicode(v_data[1][1:]), v_data[0])
     else:
-        return '0', v_data[0]
-
+        result =  ('0', v_data[0])
+    return result
 
 
 def add_data_to_dict(val: list):
-    print(check_all_condition(val))
+    key = int(check_all_condition(val)[0])
+    item = int(check_all_condition(val)[1])
+    if not values_dict.get(key):
+        values_dict[key] = item
+    else:
+        values_dict[key] += item
 
 
+def make_polynomial(data, biggest_x):
+    '''
+    начиная с питона 3.7 словари стали упорядоченными, 
+    поэтому порядок задан при создании словаря.
+    '''
+    polynomial = ' + '.join([Task_3.make_string(i,v,biggest_x) for i, v in data.items()])
+    return polynomial
 
-for i in range(1, file_count+1):
-    if not file_exists(f"file_{i}"): #  создаем файл, если его нет и заполняем его данными
-        with open(f'file_{i}', 'w') as f:
-            data = Task_3.set_degree_values(10)
-            f.write(data)
-    with open(f'file_{i}', 'r') as f:
-        for v in f:
-            for elements in v.split(' + '): #  разделяем многочлены на отдельные элементы.
-                values = elements.split('*')
-                add_data_to_dict(values)
-                        #values_dict['1']
+def run_app(file_count, count=10):
+    for i in range(1, file_count+1):
+        if not file_exists(f"file_{i}"): #  создаем файл, если его нет и заполняем его данными
+            with open(f'file_{i}', 'w') as f:
+                data = Task_3.set_degree_values(count)
+                f.write(data)
+        with open(f'file_{i}', 'r') as f:
+            for v in f:
+                for elements in v.split(' + '): #  разделяем многочлены на отдельные элементы.
+                    values = elements.split('*')
+                    add_data_to_dict(values)
+    result = make_polynomial(values_dict, count)
+    print(result, '= 0')
+    return result
 
-            #if values_dict.get()
 
-print(values_dict)
-
-
+run_app(2, 10)
